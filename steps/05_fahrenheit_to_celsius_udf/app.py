938a95c10a928283b9b97b320a36a45a4c46aad5
narashil <narayan@virginia.edu>
2023-08-15 14:18:52 -0400

    Chnages to UDF

--- steps/05_fahrenheit_to_celsius_udf/app.py
+++ steps/05_fahrenheit_to_celsius_udf/app.py
@@ -10,9 +10,10 @@
 # SNOWFLAKE ADVANTAGE: SnowCLI (PuPr)
 
 import sys
+from scipy.constants import convert_temperature
 
 def main(temp_f: float) -> float:
-    return (float(temp_f) - 32) * (5/9)
+    return convert_temperature(float(temp_f), 'F', 'C')
 
 
 # For local debugging
@@ -21,4 +22,4 @@ if __name__ == '__main__':
     if len(sys.argv) > 1:
         print(main(*sys.argv[1:]))  # type: ignore
     else:
-        print(main())  # type: ignore
+        print(main(35))  # type: ignore
